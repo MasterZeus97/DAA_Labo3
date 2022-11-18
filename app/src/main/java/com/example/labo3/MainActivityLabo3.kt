3988fd804ee29a2ae56ac4564606c29cd8284bfe
MasterZeus97 <thibault.seem@heig-vd.ch>
2022-11-18 09:53:51 +0100

    Added menu

---
+++ app/src/main/java/com/example/labo3/MainActivityLabo3.kt
@@ -0,0 +1,26 @@
+package com.example.labo3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivityLabo3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_smarphone,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.main_menu_action->{true}
+            R.id.main_menu_action2->{true}
+            else->super.onOptionsItemSelected(item)
+        }
+    }
+}
