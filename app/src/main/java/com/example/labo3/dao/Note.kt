31b149dbed7c0f4238aa3c771d81908d70c36eab
Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
2022-12-05 15:35:23 +0100

    Write DAOs

---
+++ app/src/main/java/com/example/labo3/dao/Note.kt
@@ -0,0 +1,35 @@
+package com.example.labo3.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.labo3.models.Note
+import com.example.labo3.models.State
+
+@Dao
+interface Note {
+    @Insert
+    fun insert(note: Note) : Long
+
+    @Update
+    fun update(note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+    @Query("SELECT * FROM Note")
+    fun getAllNotes() : LiveData<List<Note>>
+
+    @Query("SELECT * FROM Note WHERE noteId = :noteId")
+    fun getNoteById(noteId: Long) : Note
+
+    @Query("SELECT * FROM Note WHERE state = :state")
+    fun getNoteByState(state: State) : LiveData<List<Note>>
+
+    @Query("DELETE FROM Note") fun deleteAll()
+
+    @Query("SELECT COUNT(*) FROM Note") fun count(): LiveData<Int>
+}
